3d3294d96f599bfae9e4073edf162e498637ead5
erinspace <erin.braswell@gmail.com>
2016-09-08 09:22:49 -0600

    Make a badly placed AJax call to get citation info

--- app/components/preprint-citation.js
+++ app/components/preprint-citation.js
@@ -1,56 +1,27 @@
 import Ember from 'ember';
 
 
-var BASE_URL = '/preprints/assets/';
-
 var STYLES = {
-    apa: 'apa.csl',
-    mla: 'modern-language-association.csl',
-    chicago: 'chicago-author-date.csl'
+    apa: 'apa',
+    mla: 'modern-language-association',
+    chicago: 'chicago-author-date'
 };
 
-
-var get_sys = function(csl, locale) {
-    return {
-        retrieveItem: function(id) {
-            return csl[id];
-        },
-        retrieveLocale: function() {
-            return locale;
-        }
-    }
-};
-
-var makeCiteproc = function(style, citations, format, locale) {
-    format = format || 'html';
-    let sys = get_sys(citations, locale);
-    let citeproc = new CSL.Engine(sys, style); // jshint ignore:line
-    citeproc.setOutputFormat(format);
-    citeproc.appendCitationCluster({
-        citationItems: Object.keys(citations).map(function(key) {
-            return {
-                id: key
-            };
-        }),
-        properties: {
-            noteIndex: 0
-        }
-    });
-    return citeproc;
-};
 
 export default Ember.Component.extend({
 
     apa: Ember.computed('csl', function() {
-        return Ember.RSVP.all([
-            Ember.$.get(BASE_URL + 'locales-en-US.xml'),
-            Ember.$.get(BASE_URL + STYLES.apa)
-        ]).then((abc) => {
-            let locale = abc[0];
-            let style = abc[1];
-            let citeproc = makeCiteproc(style, this.get('csl'), 'html', locale);
-            return citeproc.makeBibliography()[1];
+
+        var csl = this.get('csl');
+        return Ember.$.ajax({
+            type: 'GET',
+            url: 'http://localhost:8000/v2/nodes/' + csl.id + '/citation/apa/',
+            crossDomain: true,
+        }).then(function (results) {
+            // debugger;
+            return results.citation;
         });
+
     }),
 
     mla: Ember.computed('csl', function() {
